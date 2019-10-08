04a0cff73e4eec96ceb37ebb62d6d24e5ed80fee
jiangyu <jiangyuwise@gmail.com>
2019-10-08 19:42:40 +0800

    添加批量删除功能

--- src/main/java/com/codve/Uploader.java
+++ src/main/java/com/codve/Uploader.java
@@ -4,8 +4,14 @@ import com.baidubce.Protocol;
 import com.baidubce.auth.DefaultBceCredentials;
 import com.baidubce.services.bos.BosClient;
 import com.baidubce.services.bos.BosClientConfiguration;
+import com.baidubce.services.bos.model.BosObjectSummary;
+import com.baidubce.services.bos.model.DeleteMultipleObjectsRequest;
+import com.baidubce.services.bos.model.ListObjectsRequest;
+import com.baidubce.services.bos.model.ListObjectsResponse;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Uploader {
 
@@ -14,6 +20,8 @@ public class Uploader {
     private String endPoint;
     private String bucket;
     private BosClient client;
+    private DeleteMultipleObjectsRequest deleteRequest;
+    private ListObjectsRequest listRequest;
 
     public Uploader(String accessId, String accessSecret, String endPoint, String bucket) {
         this.accessId = accessId;
@@ -21,6 +29,8 @@ public class Uploader {
         this.endPoint = endPoint;
         this.bucket = bucket;
         setClient();
+        setDeleteRequest();
+        setListRequest();
     }
 
     public void setClient() {
@@ -36,11 +46,53 @@ public class Uploader {
         return client;
     }
 
+    private void setDeleteRequest() {
+        this.deleteRequest = new DeleteMultipleObjectsRequest();
+        this.deleteRequest.setBucketName(bucket);
+    }
+
+    public DeleteMultipleObjectsRequest getDeleteRequest() {
+        return deleteRequest;
+    }
+
+    public void setListRequest() {
+        this.listRequest = new ListObjectsRequest(bucket);
+        this.listRequest.setMaxKeys(1000);
+    }
+
+    public ListObjectsRequest getListRequest() {
+        return listRequest;
+    }
+
+
     public void putFile(String key, File file){
         getClient().putObject(bucket, key, file);
     }
 
     public void deleteFile(String key){
         getClient().deleteObject(bucket, key);
+    }
+
+    public void deleteFiles(List<String> files) {
+        getDeleteRequest().setObjectKeys(files);
+        getClient().deleteMultipleObjects(getDeleteRequest());
+    }
+
+    public List<String> listFiles() {
+        List<String> files = new ArrayList<>();
+        ListObjectsResponse response;
+        boolean isTruncated = true;
+        while (isTruncated) {
+            response = getClient().listObjects(getListRequest());
+            for (BosObjectSummary summary : response.getContents()) {
+                files.add(summary.getKey());
+            }
+            isTruncated = response.isTruncated();
+            if (response.getNextMarker() != null) {
+                getListRequest().withMarker(response.getNextMarker());
+            }
+        }
+        getListRequest().setMarker("");
+        return files;
     }
 }
--- src/test/java/com/codve/UploaderTest.java
+++ src/test/java/com/codve/UploaderTest.java
@@ -1,12 +1,17 @@
 package com.codve;
 
 import com.baidubce.services.bos.BosClient;
+import com.baidubce.services.bos.model.DeleteMultipleObjectsRequest;
+import com.baidubce.services.bos.model.ListObjectsRequest;
+import com.baidubce.services.bos.model.ListObjectsResponse;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -81,6 +86,43 @@ public class UploaderTest {
         doReturn(client).when(uploader).getClient();
         doThrow(Exception.class).when(client).deleteObject(bucket, key);
         uploader.deleteFile(key);
+    }
+
+    /**
+     * 批量删除文件
+     */
+    @Test
+    public void deleteFiles() {
+        List<String> files = new ArrayList<>();
+        files.add("file1");
+        files.add("file2");
+        DeleteMultipleObjectsRequest request = new DeleteMultipleObjectsRequest();
+        request.setBucketName(bucket);
+        request.setObjectKeys(files);
+        doReturn(client).when(uploader).getClient();
+        doReturn(request).when(uploader).getDeleteRequest();
+        uploader.deleteFiles(files);
+
+        verify(client, times(1)).deleteMultipleObjects(request);
+    }
+
+    /**
+     * 列出 bucket 下面的所有文件
+     */
+    @Test
+    public void listFiles() {
+        ListObjectsRequest request = new ListObjectsRequest(bucket);
+        request.setMaxKeys(1000);
+        doReturn(client).when(uploader).getClient();
+        doReturn(request).when(uploader).getListRequest();
+        uploader.listFiles();
+
+        ListObjectsResponse response = mock(ListObjectsResponse.class);
+        response.setTruncated(false);
+        doReturn(request).when(client).listObjects(request);
+
+        verify(client, times(1)).listObjects(request);
+
     }
 
 }
